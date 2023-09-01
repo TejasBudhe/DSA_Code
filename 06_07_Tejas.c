6bc5042f395a0b65ddbf404919f86b4c1843ae29
TejasBudhe <141352715+TejasBudhe@users.noreply.github.com>
2023-09-01 09:55:05 +0530

    Update 06_07_Tejas.c

--- 06_07_Tejas.c
+++ 06_07_Tejas.c
@@ -1,13 +1,10 @@
-Q7 Define a structure data type called time_struct containing 3 members int hour, int
-minute and int second. Write functions that accept the structure to perform the
-following operations:
-1. Input new time
-2. Display the time
-3. Update the time such that the function accepts the time_struct and
-increments the time by one second.(If the increment results in 60 seconds, the then
-second member is set to 0 and the minute is incremented by 1. Then, if the result is 60
-minutes, the minute member is set to 0 and the hour member is incremented by 1.
-Finally when the hour becomes 24, it is set to zero.)
+/*
+name = Tejas Budhe
+Roll No. = 06
+program id = 07
+SE-IT (sem 3)
+2023-24
+*/
 
 
 #include<stdio.h>
