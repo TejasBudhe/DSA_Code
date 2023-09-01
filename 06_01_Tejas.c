4accec539b7be35cba9f79782f458b88764e03b7
TejasBudhe <141352715+TejasBudhe@users.noreply.github.com>
2023-09-01 10:34:10 +0530

    Create 06_01_Tejas.c

---
+++ 06_01_Tejas.c
@@ -0,0 +1,46 @@
+/*
+name = Tejas Budhe
+Roll No. = 06
+program id = 01
+SE-IT (sem 3)
+2023-24
+*/
+
+#include<stdio.h>
+
+/*function that returns nth term of fibonnacci series*/
+int fib(int n)
+{
+    if((n==1)||(n==2))
+    {
+        return 1;
+    }
+    return fib(n-1)+fib(n-2);
+}
+
+void main()
+{
+    /*taking input of the number that needs to be checked*/
+    int num;
+    printf("enter the number you want to check for: ");
+    scanf("%d", &num);
+
+    /*checking it is part of fibonnacci series or not*/
+    for(int i=1;; i++)
+    {
+        if(num==fib(i))
+        {
+            
+            printf("True");
+            printf("\nThe number %d is a part of fibonacci series and it is %dth in the series", num, i);
+            break;
+        }
+        if(num<fib(i))
+        {
+            /
+            printf("False");
+            printf("\nThe number %d is not a part of fibonacci series", num);
+            break;
+        }
+    }
+}
